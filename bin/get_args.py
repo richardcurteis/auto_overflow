8007cee99e71f3fbf526027626a79fc94480cff3
Richard Curteis <richardcurteis@gmail.com>
2019-04-09 19:19:10 +0100

    simplified arguments

--- bin/get_args.py
+++ bin/get_args.py
@@ -5,15 +5,15 @@ import argparse
 def input_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--ip',
+    parser.add_argument('-ip',
                         type=str,
                         required=True)
 
-    parser.add_argument('--port',
+    parser.add_argument('-port',
                         type=str,
                         required=True)
 
-    parser.add_argument('--path',
+    parser.add_argument('-path',
                         type=str,
                         required=True,
                         default='/')
